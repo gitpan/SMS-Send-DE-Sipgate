c5e44b17d4a5b7454c137d6633d3e538dbe3ca81
Dominik Schulz <tex@cpan.org>
2012-07-31 08:06:06 +0000

    Import of TEX/SMS-Send-DE-Sipgate-0.02 from CPAN.
    gitpan-cpan-distribution: SMS-Send-DE-Sipgate
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TEX/SMS-Send-DE-Sipgate-0.02.tar.gz
    gitpan-cpan-author:       TEX
    gitpan-cpan-maturity:     released

--- lib/SMS/Send/DE/Sipgate.pm
+++ lib/SMS/Send/DE/Sipgate.pm
@@ -15,11 +15,11 @@ SMS::Send::DE::Sipgate - An SMS::Send driver for the sipgate.de service.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
